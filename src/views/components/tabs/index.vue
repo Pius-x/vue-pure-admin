d59633c83b52d12b1bb01e1fba2d482840cfc272
Pius-x <1105317070@qq.com>
2022-04-21 20:01:37 +0800

    fix: 多标签组件优化

--- src/views/components/tabs/index.vue
+++ src/views/components/tabs/index.vue
@@ -1,41 +1,26 @@
 <template>
   <div>
     <el-tabs v-model="curView" type="border-card">
-      <el-tab-pane v-for="item in tabList" :label="item.label" :key="item.key" :name="item.key" />
+      <el-tab-pane v-for="(item, index) in tabList" :label="item.label" :key="index" :name="index" />
       <!--      动态组件变化时不会销毁组件-->
       <!--      <keep-alive>-->
-      <component :is="curView" />
+      <component :is="tabList[curView].component" />
       <!--      </keep-alive>-->
     </el-tabs>
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import draggable from "./subview/draggable.vue";
 import myHome from "./subview/home.vue";
 import videoComp from "./subview/video.vue";
 import { ref } from "vue";
 
-export default {
-  components: {
-    draggable,
-    myHome,
-    videoComp
-  },
+const tabList: { component: any; label: string }[] = [
+  { component: myHome, label: "主页" },
+  { component: draggable, label: "拖拽列表" },
+  { component: videoComp, label: "视频" }
+];
 
-  setup() {
-    const tabList: { key: string; label: string }[] = [
-      { key: "myHome", label: "主页" },
-      { key: "draggable", label: "拖拽列表" },
-      { key: "videoComp", label: "视频" }
-    ];
-
-    const curView = ref(tabList[0]["key"]);
-
-    return {
-      curView,
-      tabList
-    };
-  }
-};
+const curView = ref(0);
 </script>
